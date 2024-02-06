07d4836aa67eb173e91e944c6b1c43d5932cac13
eric capiz <ericcapiz@gmail.com>
2024-02-06 16:07:05 -0600

    added imgs to repo/ started on homepg

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image rc="/about.jpg" alt="" fill />
+      </div>
+    </div>
+  );
 };
 
 export default About;
