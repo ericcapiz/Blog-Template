868a8f266f51914a21015cbdd935b30f0e52615a
eric capiz <ericcapiz@gmail.com>
2024-02-07 09:58:39 -0600

    contact page added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,34 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/contact.png"
+          alt="contact img"
+          fill
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Phone" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
