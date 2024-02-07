855db2d2994fae85288e68a78c87fa4595e54a18
eric capiz <ericcapiz@gmail.com>
2024-02-07 09:45:32 -0600

    about page added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,34 @@ import styles from "./about.module.css";
 
 const About = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>
+          All Bout things that are bigger, bolder, better and simply....a breeze
+        </h1>
+        <p className={styles.desc}>
+          Stay up-to-date on the latest everything reagarding me, EZ Breezy!!
+          From my favorite sports teams and news, to food and other Chicago
+          related items. Will kee work related content to a minimum. All day
+          every day is a breeze.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10+</h1>
+            <p>Years of blogging</p>
+          </div>
+          <div className={styles.box}>
+            <h1>3k+</h1>
+            <p>Followers</p>
+          </div>
+          <div className={styles.box}>
+            <h1>Fav Teams</h1>
+            <p>Cubs/Bulls/Bears</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image rc="/about.jpg" alt="" fill />
+        <Image src="/about.png" alt="" fill className={styles.img} />
       </div>
     </div>
   );
